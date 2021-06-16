1d02746bc50d5bfcf691febb5bf9fa1e893bcc86
kazumasasama <takaik@yahoo.com>
2021-06-16 13:48:53 +0900

    Part４: Create, Update, Delete編

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,10 +15,12 @@
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
-import Vue from 'vue'
-import App from './App.vue'
+import Vue    from 'vue'
+import App    from './App.vue'
+import Router from '../router/router.js'
 
 const app = new Vue({
   el: '#app',
+  router: Router,
   render: h => h(App)
 })
